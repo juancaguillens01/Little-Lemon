62a2e665c78a913b909978449fcfddad81029fd3
juancaguillens01 <jguillenw@alumnos.unex.es>
2024-11-16 12:12:00 +0100

    Created NotImplemented component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../Home/Home';
-import Reservations from '../Reservation/Reservation';
+import Reservation from '../Reservation/Reservation';
+import NotImplemented from '../NotImplemented/NotImplemented';
 
 function Main() {
     return (
         <main className="w-100">
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/reservations" element={<Reservations />} />
+                <Route path="/reservations" element={<Reservation />} />
+                <Route path="/about" element={<NotImplemented />} />
+                <Route path="/menu" element={<NotImplemented />} />
+                <Route path="/orderOnline" element={<NotImplemented />} />
+                <Route path="/login" element={<NotImplemented />} />
             </Routes>
         </main>
     );
---
+++ src/components/NotImplemented/NotImplemented.js
@@ -0,0 +1,15 @@
+function NotImplemented() {
+    return (
+        <div className="d-flex flex-column align-items-center min-vh-100 background-green">
+            <h1 className="mt-3 color-yellow">Not implemented yet</h1>
+            <img
+                src="images/notImplemented.jpg"
+                className="img-fluid rounded-3 mt-3 mb-5"
+                alt="not implemented"
+                title="not implemented"
+            />
+        </div>
+    );
+}
+
+export default NotImplemented;
