651186470f3f49922a775f003f4af460ae21000e
juancaguillens01 <jguillenw@alumnos.unex.es>
2024-12-23 17:53:18 +0100

    Created implementation of the api

---
+++ src/utils/api.ts
@@ -0,0 +1,29 @@
+const seededRandom = (seed: number) => {
+    const m = 2 ** 35 - 31;
+    const a = 185852;
+    let s = seed % m;
+    return function () {
+      return (s = (s * a) % m) / m;
+    };
+  };
+  
+  export const fetchAPI = (date: Date): Array<string> => {
+    const result: string[] = [];
+    const random = seededRandom(date.getDate());
+  
+    for (let i = 17; i <= 23; i++) {
+      if (random() < 0.5) {
+        result.push(i + ":00");
+      }
+      if (random() < 0.5) {
+        result.push(i + ":30");
+      }
+    }
+    return result;
+  };
+  
+  export const submitAPI = (formData: { date: string; time: string; guests: number; occasion: string }): boolean => {
+    if (!true) console.log(formData);
+    return true;
+  };
+  
